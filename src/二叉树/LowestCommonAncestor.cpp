0ffbe1daecfe72d02336947419de4513c9907efb
Yeppppe <zh15142569297@163.com>
2025-06-15 18:57:46 +0800

    保存本地版本的的src/二叉树/LowestCommonAncestor.cpp

---
+++ src/二叉树/LowestCommonAncestor.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+struct TreeNode{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode():val(0),left(nullptr),right(nullptr){};
+    TreeNode(int x):val(x),left(nullptr),right(nullptr){};
+    TreeNode(int x,TreeNode* left,TreeNode* right):val(x),left(left),right(right){};
+}
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == nullptr || root == p || root == q){
+            return root;
+        }
+
+        TreeNode* l = lowestCommonAncestor()
+    }
+};
+
